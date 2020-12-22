6e0adfd71b9106b1f9c379afb681556f9f820f73
AMIT\AMIT <amitsalunke01@gmail.com>
2020-12-23 02:16:10 +0530

    Added logic for sort and hide and show of list

--- app/src/main/java/com/amitsalunke/todolistapp/data/TaskDao.kt
+++ app/src/main/java/com/amitsalunke/todolistapp/data/TaskDao.kt
@@ -1,6 +1,7 @@
 package com.amitsalunke.todolistapp.data
 
 import androidx.room.*
+import com.amitsalunke.todolistapp.ui.tasks.SortOrder
 import kotlinx.coroutines.flow.Flow
 
 @Dao
@@ -14,7 +15,18 @@ interface TaskDao {
     @Delete
     suspend fun delete(task: Task)
 
-    //|| append operator in sql lite
-    @Query("SELECT * FROM task_table WHERE name LIKE '%' || :searchQuery || '%' ORDER BY important DESC")
-    fun getTasks(searchQuery: String): Flow<List<Task>>
+    //|| append operator in sql lite  // if hide completed is true show all uncompleted task and vice versa but when it is false we need to show all data with completed and non completed is done by = 0 i.e is false
+    @Query("SELECT * FROM task_table WHERE (completed != :hideCompleted OR completed = 0) AND  name LIKE '%' || :searchQuery || '%' ORDER BY important DESC,name")
+    fun getTasksSortedByName(searchQuery: String, hideCompleted: Boolean): Flow<List<Task>>
+
+    @Query("SELECT * FROM task_table WHERE (completed != :hideCompleted OR completed = 0) AND  name LIKE '%' || :searchQuery || '%' ORDER BY important DESC,created")
+    fun getTasksSortedByDateCreated(searchQuery: String, hideCompleted: Boolean): Flow<List<Task>>
+
+
+    fun getTasks(query: String, sortOrder: SortOrder, hideCompleted: Boolean) : Flow<List<Task>> =
+        when(sortOrder){
+            SortOrder.BY_DATE -> getTasksSortedByDateCreated(query,hideCompleted)
+            SortOrder.BY_NAME -> getTasksSortedByName(query,hideCompleted)
+        }
 }
+
--- app/src/main/java/com/amitsalunke/todolistapp/ui/tasks/TaskViewModel.kt
+++ app/src/main/java/com/amitsalunke/todolistapp/ui/tasks/TaskViewModel.kt
@@ -5,16 +5,43 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import com.amitsalunke.todolistapp.data.TaskDao
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.flatMapLatest
 
 class TaskViewModel @ViewModelInject constructor(private val taskDao: TaskDao) : ViewModel() {
 
     val searchQuery = MutableStateFlow("")
 
+    //logic for sorting by name and date and hide completed
+    val sortOrder = MutableStateFlow(SortOrder.BY_DATE)
+
+    val hideCompleted = MutableStateFlow(false)
+
+/*
     //flatMapLatest is a flow operator so whenever we type search query it will execute the following block (it) is the current value of the search query (so basically we switch tracks in flow) (
     // same is available for live data and the operator is switch map)
     private val tasksFlow = searchQuery.flatMapLatest {
         taskDao.getTasks(it)
     }
     val tasks = tasksFlow.asLiveData()
+*/
+
+    //will combine multiple flow into one
+    private val tasksFlow = combine(
+        searchQuery,
+        sortOrder,
+        hideCompleted
+    ) { query, sortOrder, hideCompleted ->
+        Triple(query, sortOrder, hideCompleted)
+    }.flatMapLatest { (query, sortOrder, hideCompleted) ->
+        taskDao.getTasks(query, sortOrder, hideCompleted)
+    }
+
+    val tasks = tasksFlow.asLiveData()
+
+}
+
+enum class SortOrder {
+    BY_NAME,
+    BY_DATE
 }
